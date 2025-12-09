73bcc7d7e8e941c2c6e2761a93c2f3d6c9c40ba9
aartik17 <aartik.1704@gmail.com>
2025-12-09 12:07:20 +0530

    Update build_and_push script for multiple image tags

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -2,10 +2,12 @@
 set -euo pipefail
 
 GIT_COMMIT=${GIT_COMMIT:-latest}
-IMAGE="aarti1704/devops-nodejs-app:$GIT_COMMIT"
+IMAGE_SHA="aarti1704/devops-nodejs-app:$GIT_COMMIT"
+IMAGE_LATEST="aarti1704/devops-nodejs-app:latest"
 
-echo "Building Docker Image: $IMAGE"
-docker build -t "$IMAGE" .
+echo "Building Docker Image: $IMAGE_SHA and $IMAGE_LATEST"
+docker build -t "$IMAGE_SHA" -t "$IMAGE_LATEST" .
 
-echo "Pushing Docker Image to DockerHub: $IMAGE"
-docker push "$IMAGE"
+echo "Pushing Docker Images to DockerHub"
+docker push "$IMAGE_SHA"
+docker push "$IMAGE_LATEST"
